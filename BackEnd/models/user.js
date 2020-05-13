89caf2420a91fc225dea500cba7eed01fd5d074b
Sivam94 <paramasivamvicky@gmail.com>
2020-05-13 15:07:03 +0530

    Users APIs & project structure changes

--- BackEnd/models/user.js
+++ BackEnd/models/user.js
@@ -1,38 +1,57 @@
 const mongoose = require('mongoose');
 
-const user = mongoose.Schema({
+const user = mongoose.Schema(
+  {
     token: {
-        type: String,
-        require: [true, 'token is mandatory']
+      type: String,
+      unique: true,
+      require: [true, 'token is mandatory'],
     },
     name: {
-        type: String,
-        nullable: true
+      type: String,
+      nullable: true,
     },
     phoneNumber: {
-        type: String,
-        require: [true, 'phone number is mandatory']
+      type: String,
+      require: [true, 'phone number is mandatory'],
     },
     email: {
-        type: String,
-        require: [true, 'phone number is mandatory']
+      type: String,
+      required: [true, 'Please add an email'],
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please add a valid email',
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, 'Please add a password'],
+      minlength: 6,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ['user', 'employee', 'broker', 'admin', 'superAdmin'],
+      default: 'user',
     },
     isActive: {
-        type: Boolean,
-        require: [true, 'isActive is mandatory'],
-        default: true
+      type: Boolean,
+      require: [true, 'isActive is mandatory'],
+      default: true,
     },
     createdBy: {
-        type: mongoose.Types.ObjectId,
-        ref: 'user'
+      type: mongoose.Types.ObjectId,
+      ref: 'user',
     },
     modifiedBy: {
-        type: mongoose.Types.ObjectId,
-        ref: 'user'
-    }
-},
-{
-    timestamps: true
-});
+      type: mongoose.Types.ObjectId,
+      ref: 'user',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model('user', user);
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,113 +1,119 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config()
+const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB = require('./config/db');
 
-//Models
-const Role = require('./models/roles');
-const User = require('./models/user');
-const TestData = require('./models/TestData');
+//Loading env variables
+dotenv.config({ path: './config/.env' });
+
+//DB Connection
+connectDB();
+
+//Routing files
+const users = require('./routes/users');
 
 const app = express();
 
-//variables
-const PORT =  process.env.PORT;
-const DB_STRING = process.env.DB_CONNECTION_STRING;
-
-//DB Connection
-debugger;
-console.log('---------->',DB_STRING);
-mongoose.connect(DB_STRING, { useNewUrlParser: true,  useUnifiedTopology: true}, (err)=> {if(err) {console.log(err)} console.log(`MongoDB Connected`)});
-
-//middlewares
+//Body Parser
 app.use(express.json());
+
+//Enable CORS
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Hello'));
+//Routers
+app.use('/api/users', users);
 
-app.post('/api/addRole', (req, res) => {
-    const data = req.body;
-    console.log(data);
-
-    const role = new Role(data);
-
-    role.save((err, doc) => {
-        if(err){
-            console.log(err);
-        }
-        console.log('document saved');
-    });
-
-    res.send(data);
-});
-
-app.post('/api/addUser', (req, res) => {
-    const data = req.body;
-    console.log(data);
-
-    const user = new User(data);
-
-    user.save((err, doc) => {
-        if(err){
-            console.log(err);
-        }
-        console.log('document saved');
-    });
-
-    res.send(data);
-});
-
-//GetUser
-app.get('/api/user/:token', (req, res) => {
-    const id = req.params.token;
-    
-    const user = User.findOne({token: id}, (err, user) => {
-        if(err){
-            console.log(err);
-        }
-        console.log(user);
-        res.send(user);
-    });
-});
-
-app.post('/api/testData', (req, res) => {
-    const data = req.body;
-
-    const testData = new TestData(data);
-     testData.save((error, data) => {
-         if(error) {
-             console.log(error);
-             res.send({success: false, message: "Error saving data"})
-         }
-         res.send({success: true, data: testData});
-     });
-});
-
-app.get('/api/testData', (req, res) => {
-    
-    const data = TestData.find((err, user) => {
-        if(err){
-            console.log(err);
-        }
-        console.log(user);
-        res.send(user);
-    });
-});
-
-app.get('/api/testData/:customerId', (req, res) => {
-    const id = req.params.CustomerId;
-    
-    const data = TestData.find({CustomerId: id}, (err, user) => {
-        if(err){
-            console.log(err);
-            res.send({success: false, message: 'No Record Found'});
-        }
-        console.log(user);
-        res.send({success: true, data});
-    });
-});
-
-
+const PORT = process.env.PORT || 5000;
 
 //Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+});
+
+// app.get('/', (req, res) => res.send('Hello'));
+
+// app.post('/api/addRole', (req, res) => {
+//     const data = req.body;
+//     console.log(data);
+
+//     const role = new Role(data);
+
+//     role.save((err, doc) => {
+//         if(err){
+//             console.log(err);
+//         }
+//         console.log('document saved');
+//     });
+
+//     res.send(data);
+// });
+
+// app.post('/api/addUser', (req, res) => {
+//     const data = req.body;
+//     console.log(data);
+
+//     const user = new User(data);
+
+//     user.save((err, doc) => {
+//         if(err){
+//             console.log(err);
+//         }
+//         console.log('document saved');
+//     });
+
+//     res.send(data);
+// });
+
+// //GetUser
+// app.get('/api/user/:token', (req, res) => {
+//     const id = req.params.token;
+
+//     const user = User.findOne({token: id}, (err, user) => {
+//         if(err){
+//             console.log(err);
+//         }
+//         console.log(user);
+//         res.send(user);
+//     });
+// });
+
+// app.post('/api/testData', (req, res) => {
+//     const data = req.body;
+
+//     const testData = new TestData(data);
+//      testData.save((error, data) => {
+//          if(error) {
+//              console.log(error);
+//              res.send({success: false, message: "Error saving data"})
+//          }
+//          res.send({success: true, data: testData});
+//      });
+// });
+
+// app.get('/api/testData', (req, res) => {
+
+//     const data = TestData.find((err, user) => {
+//         if(err){
+//             console.log(err);
+//         }
+//         console.log(user);
+//         res.send(user);
+//     });
+// });
+
+// app.get('/api/testData/:customerId', (req, res) => {
+//     const id = req.params.CustomerId;
+
+//     const data = TestData.find({CustomerId: id}, (err, user) => {
+//         if(err){
+//             console.log(err);
+//             res.send({success: false, message: 'No Record Found'});
+//         }
+//         console.log(user);
+//         res.send({success: true, data});
+//     });
+// });
