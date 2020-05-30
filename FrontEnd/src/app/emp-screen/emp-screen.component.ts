9d3ab197b207793642785ff1dd5bbbae68da0e62
babu <gmbabuji@gmail.com>
2020-05-30 20:43:23 +0530

    nav bar employee

--- FrontEnd/src/app/emp-screen/emp-screen.component.ts
+++ FrontEnd/src/app/emp-screen/emp-screen.component.ts
@@ -7,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpScreenComponent implements OnInit {
 
-  
+
   ngOnInit() {
   }
- 
 
+  selectedElement() {
+    const lineElement: any =  document.querySelectorAll('.line-vertical')[0];
+    setTimeout(() => {
+      const el: any = document.querySelectorAll('#sidebar ul > .active');
+      let postion = el[0].offsetTop || 0;
+      const offsetHeight = el[0].offsetHeight || 0;
+      postion = postion - offsetHeight;
+
+      console.log(postion);
+    //   lineElement.setStyle({
+    //     top: postion + 'px',
+    //  });
+      lineElement.style.top = postion + 'px';
+    }, 100);
+
+  }
 
 }
